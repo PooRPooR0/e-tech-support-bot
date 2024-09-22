373be15b350368472258fbf5d97543446cd0bb8a
Oleg Kasenbekov <oleg.kasenbekov@enterra-inc.com>
2024-09-22 15:43:25 +0700

    action translations fix

--- src/utils/get-user-lang.js
+++ src/utils/get-user-lang.js
@@ -1,7 +1,7 @@
 const {languages} = require("./translations");
 
 const getUserLang = (ctx) => {
-    let userLang = ctx.update.message?.from?.language_code || 'en'
+    let userLang = ctx.update.message?.from?.language_code || ctx.update.callback_query?.from?.language_code || 'en'
     if (!languages.includes(userLang)) userLang = 'en'
 
     return userLang
